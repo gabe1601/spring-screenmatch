0c9804afaf2da7d8c5026a175f895fee5f1f73cb
Gabriel Pereira <gabrielpereira160101@gmail.com>
2024-04-16 21:03:11 -0300

    crianodo uma estrutura Java Web

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -6,5 +6,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("Genre") String categoria,
+                         @JsonAlias("Actors") String atores,
+                         @JsonAlias("Poster") String imagem,
+                         @JsonAlias("Plot") String sinopse,
                          @JsonAlias("imdbRating") String avaliacao) {
 }
