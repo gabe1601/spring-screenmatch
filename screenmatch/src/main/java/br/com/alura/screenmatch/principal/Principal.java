93a56ef9a1c12268c1e3bc0227883d53a854474d
Gabriel Pereira <gabrielpereira160101@gmail.com>
2024-05-17 20:23:36 -0300

    Criado o método de busca por nome.

--- screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -38,6 +38,7 @@ public class Principal {
                     1- Buscar séries
                     2- Buscar episódios
                     3- Listar séries buscadas
+                    4- Buscar série por título
                                     
                     0- Sair
                     """;
@@ -56,6 +57,9 @@ public class Principal {
                 case 3:
                     listarSeriesBuscadas();
                     break;
+                case 4:
+                    buscarSeriePorTitulo();
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -65,7 +69,7 @@ public class Principal {
         }
         }
 
-        private void buscarSerieWeb() {
+    private void buscarSerieWeb() {
             DadosSerie dados = getDadosSerie();
             Serie serie = new Serie(dados);
             repositorio.save(serie);
@@ -86,10 +90,7 @@ public class Principal {
             System.out.print("Escolha uma série pelo nome: ");
             var nomeSerie = leitura.nextLine();
 
-            Optional<Serie> serie = series.
-                    stream().
-                    filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
-                    .findFirst();
+            Optional<Serie> serie = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
 
             if(serie.isPresent()) {
 
@@ -121,4 +122,16 @@ public class Principal {
            series.stream().sorted(Comparator.comparing(Serie::getGenero))
                    .forEach(System.out::println);
         }
+
+    private void buscarSeriePorTitulo() {
+        System.out.print("Escolha uma série pelo nome: ");
+        var nomeSerie = leitura.nextLine();
+        Optional<Serie>serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+
+        if(serieBuscada.isPresent()){
+            System.out.println("Dados da série: " + serieBuscada.get());
+        }else{
+            System.out.println("Série não encontrada!");
+        }
+    }
 }
