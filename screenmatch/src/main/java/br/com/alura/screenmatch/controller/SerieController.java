808302af655d3ab57aa38a157aa527d637ca1d9d
Gabriel Pereira <gabrielpereira160101@gmail.com>
2024-07-11 21:03:28 -0300

    Criando a classe servicos

--- screenmatch/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,26 +1,28 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.services.SerieServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
-    private SerieRepository repositorio;
+    private SerieServices servico;
 
-    @GetMapping("/series")
+    @GetMapping
     public List<SerieDTO> obterSeries(){
-        return repositorio.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(),s.getTotalTemporadas(),
-                        s.getGenero(),s.getAtores(),s.getImagem(),s.getSinopse(),s.getAvaliacao()))
-                .collect(Collectors.toList());
+        return servico.obterTodasAsSeries();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO>obterTop5Series(){
+        return servico.obterTop5Series();
     }
 }
---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/services/SerieServices.java
@@ -0,0 +1,32 @@
+package br.com.alura.screenmatch.services;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieServices {
+
+    @Autowired
+    private SerieRepository repositorio;
+
+    public List<SerieDTO> obterTodasAsSeries(){
+        return converteDados(repositorio.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series){
+        return series.stream()
+                .map(s -> new SerieDTO(s.getId(),s.getTitulo(),s.getTotalTemporadas(),
+                        s.getGenero(),s.getAtores(),s.getImagem(),s.getSinopse(),s.getAvaliacao()))
+                .collect(Collectors.toList());
+    }
+}
